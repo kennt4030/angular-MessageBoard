06acf6d2b8942ecfb7ab9f18def7495d53fce616
Kenny <kenny47201@gmail.com>
2018-03-15 17:01:13 -0400

    set up firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {Routes, RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-
-
+import { MatToolbarModule, MatButtonModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import {Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 const routes: Routes = [
   {
@@ -23,18 +26,35 @@ const routes: Routes = [
     path: '**', redirectTo: '/'
   }
 ];
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMkerOoG4MvDIdFsLvtTs3oAdIR9LKnYA",
+  authDomain: "beginnerangularproject.firebaseapp.com",
+  databaseURL: "https://beginnerangularproject.firebaseio.com",
+  projectId: "beginnerangularproject",
+  storageBucket: "",
+  messagingSenderId: "965276776866"
+
+};
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+    // MatToolbarModule,
+    // MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
